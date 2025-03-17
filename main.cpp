1995ce48666240ca5e18fab7b423adbe12d9d45d
v33sergiulica <sergiudilgeanu112@gmail.com>
2025-03-17 16:19:12 +0200

    test

--- main.cpp
+++ main.cpp
@@ -1,57 +1,81 @@
 #include <iostream>
-#include <array>
+#include <algorithm>
+/*                          0          1          2        3           4      */
+std::string suitfy[5] = {"Hearts", "Diamonds", "Clubs", "Spades", "all Suits"};
+std::string valuefy[14] = {"Joker", "Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King"};
+/*                      unused0       1     2    3    4    5    6    7    8    9    10     11      12       13   */
+class Card {
+private:
+    int value;
+    int suit;
+public:
+    int getValue() {return value;}
+    int getSuit() {return suit;}
+    Card() {value = 0; suit = 4;}
+    Card(int v, int s) {value = v; suit = s;}
+    };
 
-#include <Helper.h>
+class Deck {
+private:
+    Card cards[52];
+    // 0 -> Top of the deck
+    // Using a card puts it at the bottom of the deck
+public:
+    void shuffle() {
+        std::random_shuffle(std::begin(cards), std::end(cards));
+        std::cout << "Shuffled" << std::endl;
+    };
+    void print() {
+        for (int i = 0; i < 52; i++) {
+            std::cout << valuefy[cards[i].getValue()] << " of " << suitfy[cards[i].getSuit()] << std::endl;
+        }
+    }
+    Card popFCard() {
+        return cards[0];
+        cycleFCard();
+    }
+    void cycleFCard() {
+        int su = cards[0].getSuit();
+        int va = cards[0].getValue();
+        for (int i = 0; i < 51; i++) {
+            cards[i] = cards[i + 1]; // CC ?
+        }
+        cards[51] = Card(va, su); // CC ?
+        //std::cout << valuefy[va] << " of " << suitfy[su] << " was turned" << std::endl;
+    }
+    Deck() {
+        for (int i = 0; i < 52; i++) {
+            cards[i] = Card(i % 13 + 1, i / 13);
+        }
+    }
+};
+
+class Player {
+private:
+    Card hand[2];
+    int money;
+    bool isPlaying;
+    bool itsTurn;
+public:
+    Player(Deck &deck) {
+        hand[0] = deck.popFCard();
+        deck.cycleFCard();
+        hand[1] = deck.popFCard();
+        deck.cycleFCard();
+        money = 1000;
+        isPlaying = true;
+        itsTurn = false;
+    }
+    void printHand() {
+        std::cout << "First card is a " << valuefy[hand[0].getValue()] << " of " << suitfy[hand[0].getSuit()] << std::endl;
+        std::cout << "Second card is a " << valuefy[hand[1].getValue()] << " of " << suitfy[hand[1].getSuit()] << std::endl;
+    }
+};
 
 int main() {
-    std::cout << "Hello, world!\n";
-    std::array<int, 100> v{};
-    int nr;
-    std::cout << "Introduceți nr: ";
-    /////////////////////////////////////////////////////////////////////////
-    /// Observație: dacă aveți nevoie să citiți date de intrare de la tastatură,
-    /// dați exemple de date de intrare folosind fișierul tastatura.txt
-    /// Trebuie să aveți în fișierul tastatura.txt suficiente date de intrare
-    /// (în formatul impus de voi) astfel încât execuția programului să se încheie.
-    /// De asemenea, trebuie să adăugați în acest fișier date de intrare
-    /// pentru cât mai multe ramuri de execuție.
-    /// Dorim să facem acest lucru pentru a automatiza testarea codului, fără să
-    /// mai pierdem timp de fiecare dată să introducem de la zero aceleași date de intrare.
-    ///
-    /// Pe GitHub Actions (bife), fișierul tastatura.txt este folosit
-    /// pentru a simula date introduse de la tastatură.
-    /// Bifele verifică dacă programul are erori de compilare, erori de memorie și memory leaks.
-    ///
-    /// Dacă nu puneți în tastatura.txt suficiente date de intrare, îmi rezerv dreptul să vă
-    /// testez codul cu ce date de intrare am chef și să nu pun notă dacă găsesc vreun bug.
-    /// Impun această cerință ca să învățați să faceți un demo și să arătați părțile din
-    /// program care merg (și să le evitați pe cele care nu merg).
-    ///
-    /////////////////////////////////////////////////////////////////////////
-    std::cin >> nr;
-    /////////////////////////////////////////////////////////////////////////
-    for(int i = 0; i < nr; ++i) {
-        std::cout << "v[" << i << "] = ";
-        std::cin >> v[i];
-    }
-    std::cout << "\n\n";
-    std::cout << "Am citit de la tastatură " << nr << " elemente:\n";
-    for(int i = 0; i < nr; ++i) {
-        std::cout << "- " << v[i] << "\n";
-    }
-    ///////////////////////////////////////////////////////////////////////////
-    /// Pentru date citite din fișier, NU folosiți tastatura.txt. Creați-vă voi
-    /// alt fișier propriu cu ce alt nume doriți.
-    /// Exemplu:
-    /// std::ifstream fis("date.txt");
-    /// for(int i = 0; i < nr2; ++i)
-    ///     fis >> v2[i];
-    ///
-    ///////////////////////////////////////////////////////////////////////////
-    ///                Exemplu de utilizare cod generat                     ///
-    ///////////////////////////////////////////////////////////////////////////
-    Helper helper;
-    helper.help();
-    ///////////////////////////////////////////////////////////////////////////
-    return 0;
+    Deck deck;
+    Player player1(deck);
+    player1.printHand();
+    Player player2(deck);
+    player2.printHand();
 }
